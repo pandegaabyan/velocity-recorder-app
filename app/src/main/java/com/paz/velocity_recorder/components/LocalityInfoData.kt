cfd860d23612d3b07520c87fde42c6a123aa4ed6
pandega <pandegaabyan@gmail.com>
2023-11-24 17:13:08 +0700

    add locality info collector

---
+++ app/src/main/java/com/paz/velocity_recorder/components/LocalityInfoData.kt
@@ -0,0 +1,8 @@
+package com.paz.velocity_recorder.components
+
+data class LocalityInfoData(
+    val locality: String,
+    val city: String?,
+    val country: String,
+    val countryCode: String
+)
