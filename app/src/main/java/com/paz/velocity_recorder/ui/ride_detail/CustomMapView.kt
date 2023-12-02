16f83fedd2049d55b8251c34e795454e641c2d93
pandega <pandegaabyan@gmail.com>
2023-12-03 06:31:12 +0700

    use custom map view to prevent scroll

---
+++ app/src/main/java/com/paz/velocity_recorder/ui/ride_detail/CustomMapView.kt
@@ -0,0 +1,22 @@
+package com.paz.velocity_recorder.ui.ride_detail
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import com.google.android.gms.maps.MapView
+
+class CustomMapView(context: Context?, attrs: AttributeSet?):
+    MapView(context!!, attrs) {
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        when (ev.action) {
+            MotionEvent.ACTION_UP -> {
+                this.parent.requestDisallowInterceptTouchEvent(false)
+            }
+
+            MotionEvent.ACTION_DOWN -> {
+                this.parent.requestDisallowInterceptTouchEvent(true)
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+}
