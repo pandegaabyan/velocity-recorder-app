53c95bd53b7ccbf08a45843e993d14aa255a88b6
pandega <pandegaabyan@gmail.com>
2023-12-03 13:34:29 +0700

    refactor minor

--- app/src/main/java/com/paz/velocity_recorder/ui/ride_detail/CustomMapView.kt
+++ app/src/main/java/com/paz/velocity_recorder/ui/ride_detail/CustomMapView.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import com.google.android.gms.maps.MapView
 
-class CustomMapView(context: Context?, attrs: AttributeSet?):
+class CustomMapView(context: Context?, attrs: AttributeSet?) :
     MapView(context!!, attrs) {
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         when (ev.action) {
